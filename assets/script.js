43d47639ff4f0a80afb4fdba40a04bb78248dc55
catcueto <catacueto@gmail.com>
2022-07-11 20:39:51 -0400

    initial pseucode done

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+// PSEUDOCODE
+
+// STEP 1: Display Weather Dashbord at top of viewport
+// STEP 2: Left-side bar (FORM) that allows the user to search for a city
+// STEP 3: Selected city is added to the search history (left-side)
+/* STEP 4: When city is searched, then weather conditions are displayed (CURRENT & 5-DAY FORECAST)
+        a) User can see city name, today's date, icon representation of weather conditions, temperature, humidity, wind speed, and UV index 
+        b) When user views UV index, this should be colored according to whether the conditions are favorable (green), moderate (orange), or severe (red)
+        c) 5-day forecast IsNCLUDES same sections (date + icon representation + temp + wind + humidty */
+//  STEP 5: When clicking on a city in search history, then user can see current and future conditions for selected city AGAIN.
